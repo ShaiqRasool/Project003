7867083ec61bae4481bb2ea0a387ba6fcf51f544
ShaiqRasool <ShaiqRasool@hotmail.com>
2020-12-27 01:24:10 +0000

    added description

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-	<br/>
-	<h1>Update by Shaiq
+	  <br/>
+	  Update by Shaiq
         </p>
         <a
           className="App-link"
